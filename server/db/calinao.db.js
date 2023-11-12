6366d7c5aa5166303044cb5eaae232f55944181b
reizellnn <20-49929@g.batstate-u.edu.ph>
2023-11-12 15:02:01 +0800

    draft

--- server/db/calinao.db.js
+++ server/db/calinao.db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: "postgres",
     host: "localhost",
     database: "calinao_db",
-    password: "dyasmir",
+    password: "ccs123",
     port: 5432,
 });
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,6 @@
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import { useState } from "react";
 
 const Profilee = ({ clientData }) => {
     const [isEdit, setIsEdit] = useState(false);
